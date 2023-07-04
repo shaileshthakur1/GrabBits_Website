f20976a2b5fe9012de0c74bfbf6d7bf32270a5ba
Tisha <tishag6661@gmail.com>
2023-07-04 13:58:53 +0530

    Podcast Cards Ui Improved
    The podcast cards are made even, regardless of the content in it.

--- client/src/components/common/PodcastCard/PodcastCard.jsx
+++ client/src/components/common/PodcastCard/PodcastCard.jsx
@@ -8,24 +8,28 @@ const PodcastCard = ({ podcast = {}, show, imgPreview }) => {
 	return (
 		<>
 			<div className={classes.podcast_card}>
-				{show ? (
-					<img
-						className={classes.podcast_image}
-						src={imgPreview}
-						alt="youtube_header"
-					/>
-				) : (
-					<img
-						className={classes.podcast_image}
-						src={`data:image/png;base64, ${image}`}
-						alt="youtube_header"
-					/>
-				)}
-				<h3 className={classes.podcast_name}>{title}</h3>
-				<h4 className={classes.podcast_person}>{name}</h4>
-				<div className={classes.separator}></div>
-				<div className={classes.btn_container}>
-					<Button link={link} label="Watch Video" bgColor="#7541C8" />
+				<div className={classes.w100}>
+					{show ? (
+						<img
+							className={classes.podcast_image}
+							src={imgPreview}
+							alt="youtube_header"
+						/>
+					) : (
+						<img
+							className={classes.podcast_image}
+							src={`data:image/png;base64, ${image}`}
+							alt="youtube_header"
+						/>
+					)}
+					<h3 className={classes.podcast_name}>{title}</h3>
+					<h4 className={classes.podcast_person}>{name}</h4>
+				</div>
+				<div className={classes.w100}>
+					<div className={classes.separator}></div>
+					<div className={classes.btn_container}>
+						<Button link={link} label="Watch Video" bgColor="#7541C8" />
+					</div>
 				</div>
 			</div>
 		</>
